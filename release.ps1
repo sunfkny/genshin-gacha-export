72a2d9f205d1b9f74863ccf8dc74cd134400b077
sunfkny <30853461+sunfkny@users.noreply.github.com>
2022-02-16 16:46:43 +0800

    一键release

---
+++ release.ps1
@@ -0,0 +1,13 @@
+$version = python config.py
+
+$Answer = Read-Host -Prompt "Release $version ? (y/n)"
+
+if ($Answer -eq 'y' -or $Answer -eq 'Y') {
+    git add config.py version.txt
+    git commit -m "update version to $version"
+    git push
+    git tag -a $version -m update
+    git push --tags
+} else {
+    Write-Host "Aborted"
+}
